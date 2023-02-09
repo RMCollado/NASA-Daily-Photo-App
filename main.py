e21d67acce5e8c64e9e357aab1875088effd711d
Rich <Richardmcollado@gmail.com>
2023-02-09 18:08:35 -0500

    Basic APP

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import streamlit as st
+import requests
+from datetime import date
+
+API_KEY = os.getenv("NASA_API_KEY")
+NASA_URL = "https://api.nasa.gov/planetary/apod"
+today = date.today()
+
+today = today.strftime("%Y-%m-%d")
+
+parameters = {
+    "api_key": API_KEY,
+    "date": today,
+}
+
+response = requests.get(url=NASA_URL, params=parameters).json()
+
+
+image = response['hdurl']
+
+st.title(response['title'])
+
+img_response = requests.get(url=image).content
+st.image(img_response)
+
+
+st.write(response['explanation'])
